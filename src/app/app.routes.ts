040ff3c833b4dcc6d548267c24d6369f1546cef5
darwgom7 <darwgom7@gmail.com>
2025-04-18 00:35:23 -0500

    Adjusting deployment flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { TextTransformerComponent } from './features/text-tools/components/text-transformer/text-transformer.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'text-transformer', pathMatch: 'full' },
   { path: 'text-transformer', component: TextTransformerComponent },
-  // Agrega más rutas aquí según las nuevas funcionalidades
 ];
--- src/app/features/text-tools/text-tools.routes.ts
+++ src/app/features/text-tools/text-tools.routes.ts
@@ -3,5 +3,4 @@ import { TextTransformerComponent } from './components/text-transformer/text-tra
 
 export const textToolsRoutes: Routes = [
   { path: 'text-transformer', component: TextTransformerComponent },
-  // Puedes agregar más rutas aquí en el futuro
 ];
